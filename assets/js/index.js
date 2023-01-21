d9d468dfe2f51d3999addc538bcae87722f833d6
Tobi Olajide <tobiolajide887@yahoo.com>
2023-01-21 22:13:20 +0000

    added eventlistener to handle click of save button and function to store value of inputted event plan

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,12 @@
 // Display current time and date 
 let $currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+let $saveBtn =$("button");
+
+function saveEvent(event) {
+    let $newEvent =$(event.target).siblings("#event").val();
+    console.log($newEvent);
+    
+}
+
+// event handler for user event save button
+$saveBtn.on('click', saveEvent);

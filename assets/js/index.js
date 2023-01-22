8282fc0d0ea6769cc2fdf263930e419b9ffbd504
Tobi Olajide <tobiolajide887@yahoo.com>
2023-01-22 16:58:52 +0000

    Work in Progress: rendering saved events

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,7 @@
 let $currentDay = $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 // Button to save user inputted events into time block
 let $saveBtn =$("button");
-// Hour of the day i.e., 9am - 5pm
-let $hour =$(".hour").text();
+
 // 
 let eventTextArea = document.querySelector("#event");
 // Available timeblocks
@@ -19,21 +18,31 @@ let hours = [
     "5pm"
 ];
 
-renderSavedEvents();
+function init() {
+    $("#event").attr('data-time', $hour);
+    // renderSavedEvents();
+}
+
+
 // Function to render saved events 
 function renderSavedEvents () {
     for (let i = 0; i < hours.length; i++) {
         const hour = hours[i];
+        console.log(hour);
         let savedEvent = localStorage.getItem(hour);
         if (savedEvent !== null){
             console.log(savedEvent);
-            $("#event").text(savedEvent);
+            let test = "." + hour
+            console.log(test);
+            $("#hour").text(savedEvent);
         }    
     }
 }
 
 // Function to store new event in timeblock
 function saveEvent(event) {
+    // Hour of the day i.e., 9am - 5pm
+    let $hour =$(event.target).siblings(".hour").text();
     // store inputted event
     let $newEvent = $(event.target).siblings("#event").val();
     // store inputted event in local storage. Key is hour of day
@@ -41,7 +50,7 @@ function saveEvent(event) {
     $("#event").attr('data-time', $hour);
     // add hour to class for event text area
     $("#event").addClass($hour);
-    console.log($newEvent);
+    console.log($hour);
     
 }
 
